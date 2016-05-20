e16f34a79c5e49a3bf2ca3eac23f742683790652
jboixmogollon <javier.boix.mogollon@gmail.com>
2016-05-20 18:00:15 +0200

    id's fixed

--- Hundir La Flota/model/Jugador.vb
+++ Hundir La Flota/model/Jugador.vb
@@ -1,3 +1,12 @@
 ﻿Public Class Jugador
+    Private id1 As Integer
+    Private id2 As Integer
+    Private name As String = ""
+
+    Public Sub New()
+        Me.id1 = 1
+        Me.id2 = 2
+    End Sub
+
 
 End Class
