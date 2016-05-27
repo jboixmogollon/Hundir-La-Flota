6f9e558db482b1e57c2c7d8db9226dfaac241f16
jboixmogollon <javier.boix.mogollon@gmail.com>
2016-05-27 17:02:31 +0200

    los barcos se mueven

--- Hundir La Flota/model/Game.vb
+++ Hundir La Flota/model/Game.vb
@@ -7,7 +7,7 @@
     Public Function crearArray() As String
         For x = 0 To 9
             For y = 0 To 9
-                salida = salida + "|     "
+                salida = salida + "0     "
             Next
             salida = salida + vbNewLine
         Next
@@ -21,15 +21,13 @@
 
     Public Function updateArray() As String
         salida = ""
-        Dim c1pos1 As Integer = InputBox("Introduce X del crucero")
-        Dim c1pos2 As Integer = InputBox("Introduce Y del crucero")
-
-        Dim c2pos1 As Integer = InputBox("Introduce X del crucero")
-        Dim c2pos2 As Integer = InputBox("Introduce Y del crucero")
-
+        Dim c1pos1 As Integer = InputBox("Introduce X del crucero número 1")
+        Dim c1pos2 As Integer = InputBox("Introduce Y del crucero número 1")
+        Dim c2pos1 As Integer = InputBox("Introduce X del crucero número 2")
+        Dim c2pos2 As Integer = InputBox("Introduce Y del crucero número 2")
         For x = 0 To 9
             For y = 0 To 9
-                If x = c1pos1 And y = c2pos2 Or x = c2pos2 And y = c2pos2 Then
+                If x = c1pos1 And y = c1pos2 Or x = c2pos1 And y = c2pos2 Then
                     terreno(x, y) = 2
                 Else
                     terreno(x, y) = 0
